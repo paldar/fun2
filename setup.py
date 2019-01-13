43e136ace0d67b6e20a192fd69a678c29ac36e0e
peteraldaron <peteraldaron@gmail.com>
2019-01-13 21:49:21 +0200

    Add travis yml and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="f2",
+      version="0.0.1.dev",
+      author="Peter Aldaron",
+      author_email="peteraldaron@gmail.com",
+      url="https://github.com/peteraldaron/fun2",
+      tests_require=[],
+      py_modules=["f2"],
+      license="MIT",
+      python_requires=">=3.6")
